08a2855083b4f2dbd98e27ab6433171195a841a0
IcebemAst <2369631276@qq.com>
2019-10-22 18:24:40 +0800

    Update: Overlay layout

--- app/src/main/java/com/icebem/akt/service/OverlayService.java
+++ app/src/main/java/com/icebem/akt/service/OverlayService.java
@@ -19,6 +19,10 @@ public class OverlayService extends Service {
         super.onCreate();
         views = new OverlayView[2];
         views[0] = new OverlayView(this, LayoutInflater.from(this).inflate(R.layout.content_overlay, null), Gravity.END | Gravity.TOP, false, null);
+        views[0].getView().findViewById(R.id.action_disconnect).setOnClickListener(view -> {
+            stopSelf();
+            Toast.makeText(this, R.string.info_overlay_disconnected, Toast.LENGTH_LONG).show();
+        });
         views[0].getView().findViewById(R.id.action_collapse).setOnClickListener(view -> {
             views[0].remove();
             views[1].show();
@@ -31,7 +35,7 @@ public class OverlayService extends Service {
                 views[0].show();
             }
         }).show();
-        Toast.makeText(this, R.string.info_overlay_showing, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.info_overlay_connected, Toast.LENGTH_LONG).show();
     }
 
     @Override
