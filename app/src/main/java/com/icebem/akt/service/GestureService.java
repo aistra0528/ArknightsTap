00a878d36abb9492d7edceed901db3629156564f
IcebemAst <2369631276@qq.com>
2020-05-20 19:29:32 +0800

    Bug fix

--- app/src/main/java/com/icebem/akt/service/GestureService.java
+++ app/src/main/java/com/icebem/akt/service/GestureService.java
@@ -7,14 +7,12 @@ import android.content.IntentFilter;
 import android.graphics.Path;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
-import androidx.annotation.NonNull;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.icebem.akt.BuildConfig;
@@ -29,11 +27,13 @@ import java.lang.ref.WeakReference;
 
 public class GestureService extends AccessibilityService {
     private static final int GESTURE_DURATION = 120;
-    private static final long LONG_MIN = 60000;
+    private static final long MINUTE_TIME = 60000;
     private static final String THREAD_GESTURE = "gesture";
     private static final String THREAD_TIMER = "timer";
     private int time;
     private boolean running;
+    private Handler handler;
+    private Thread gestureThread, timerThread;
     private PreferenceManager manager;
     private GestureActionReceiver gestureActionReceiver;
     private LocalBroadcastManager localBroadcastManager;
@@ -48,6 +48,7 @@ public class GestureService extends AccessibilityService {
             return;
         }
         ((BaseApplication) getApplication()).setGestureService(this);
+        handler = new Handler(Looper.getMainLooper());
         gestureActionReceiver = new GestureActionReceiver(this::dispatchCurrentAction);
         localBroadcastManager = LocalBroadcastManager.getInstance(this);
         localBroadcastManager.registerReceiver(gestureActionReceiver, new IntentFilter(GestureActionReceiver.ACTION));
@@ -66,18 +67,26 @@ public class GestureService extends AccessibilityService {
         running = true;
         if (manager.launchGame())
             launchGame();
-        new Thread(this::performGestures, THREAD_GESTURE).start();
+        if (gestureThread == null || !gestureThread.isAlive())
+            gestureThread = new Thread(this::performGestures, THREAD_GESTURE);
+        if (!gestureThread.isAlive())
+            gestureThread.start();
         time = manager.getTimerTime();
         if (time > 0) {
-            Handler handler = new TimerHandler();
-            new Thread(() -> {
-                while (running && time > 0) {
-                    handler.sendEmptyMessage(time);
-                    SystemClock.sleep(LONG_MIN);
-                    time--;
-                }
-                stopAction();
-            }, THREAD_TIMER).start();
+            if (timerThread == null || !timerThread.isAlive()) {
+                timerThread = new Thread(() -> {
+                    while (running && time > 0) {
+                        handler.post(this::showTimeLeft);
+                        SystemClock.sleep(MINUTE_TIME);
+                        time--;
+                    }
+                    stopAction();
+                }, THREAD_TIMER);
+            }
+            if (timerThread.isAlive())
+                showTimeLeft();
+            else
+                timerThread.start();
         } else
             OverlayToast.show(this, R.string.info_gesture_connected, OverlayToast.LENGTH_SHORT);
     }
@@ -116,11 +125,15 @@ public class GestureService extends AccessibilityService {
                 SystemClock.sleep(RandomUtil.randomT(manager.getUpdateTime()));
             }
         }
-        new Handler(Looper.getMainLooper()).post(this::showActionFinished);
+        handler.post(this::showActionFinished);
     }
 
     private void showActionFinished() {
         OverlayToast.show(this, R.string.info_gesture_disconnected, OverlayToast.LENGTH_SHORT);
+    }
+
+    private void showTimeLeft() {
+        OverlayToast.show(this, getString(R.string.info_gesture_running, time), OverlayToast.LENGTH_SHORT);
     }
 
     private void launchGame() {
@@ -143,6 +156,18 @@ public class GestureService extends AccessibilityService {
     }
 
     @Override
+    public boolean onUnbind(Intent intent) {
+        stopAction();
+        if (localBroadcastManager != null)
+            localBroadcastManager.unregisterReceiver(gestureActionReceiver);
+        else if (!Settings.canDrawOverlays(this))
+            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        else if (manager.unsupportedResolution())
+            OverlayToast.show(this, R.string.state_resolution_unsupported, OverlayToast.LENGTH_SHORT);
+        return super.onUnbind(intent);
+    }
+
+    @Override
     protected boolean onKeyEvent(KeyEvent event) {
         if (running && event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
             pauseAction();
@@ -158,30 +183,5 @@ public class GestureService extends AccessibilityService {
         if (currentInstance != null && currentInstance.get() != null)
             return currentInstance.get().running;
         return false;
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        stopAction();
-        if (localBroadcastManager != null)
-            localBroadcastManager.unregisterReceiver(gestureActionReceiver);
-        else if (!Settings.canDrawOverlays(this))
-            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        else if (manager.unsupportedResolution())
-            OverlayToast.show(this, R.string.state_resolution_unsupported, OverlayToast.LENGTH_SHORT);
-        return super.onUnbind(intent);
-    }
-
-    private static final class TimerHandler extends Handler {
-        private TimerHandler() {
-            super(Looper.getMainLooper());
-        }
-
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            if (currentInstance != null && currentInstance.get() != null)
-                OverlayToast.show(currentInstance.get(), currentInstance.get().getString(R.string.info_gesture_running, msg.what), OverlayToast.LENGTH_SHORT);
-        }
     }
 }
