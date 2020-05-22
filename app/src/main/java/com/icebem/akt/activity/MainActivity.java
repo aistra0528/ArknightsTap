c187ef9daad7873062cb8729ca574fdeec7d11fb
twiceYuan <twiceyuan@gmail.com>
2020-05-23 07:39:46 +0800

    Feature: Root compatible mode below Android 7.0

--- app/src/main/java/com/icebem/akt/activity/MainActivity.java
+++ app/src/main/java/com/icebem/akt/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.icebem.akt.activity;
 
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -18,9 +17,13 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.icebem.akt.R;
+import com.icebem.akt.app.CompatOperations;
 import com.icebem.akt.app.PreferenceManager;
 import com.icebem.akt.service.OverlayService;
 import com.icebem.akt.util.AppUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private TextView subtitle;
@@ -47,10 +50,12 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(v -> showOverlay());
         if (manager.isPro())
             fab.setOnLongClickListener(this::showAccessibilitySettings);
+        if (manager.rootCompatible())
+            CompatOperations.checkRootPermission();
     }
 
     public void showOverlay() {
-        if (Settings.canDrawOverlays(this)) {
+        if (CompatOperations.canDrawOverlays(this)) {
             startService(new Intent(this, OverlayService.class));
         } else {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -68,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateSubtitleTime() {
-        subtitle.setText(new SimpleDateFormat(AppUtil.DATE_FORMAT).format(manager.getCheckLastTime()));
+        subtitle.setText(new SimpleDateFormat(AppUtil.DATE_FORMAT, Locale.getDefault()).format(manager.getCheckLastTime()));
     }
 
     private void onDestinationChanged(NavDestination destination) {
