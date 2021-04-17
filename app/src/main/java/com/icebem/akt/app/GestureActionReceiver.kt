0816b537023454d08b57eee9bc85aa2839791b69
IcebemAst <2369631276@qq.com>
2021-04-17 22:03:14 +0800

    refactor: J2K

--- app/src/main/java/com/icebem/akt/app/GestureActionReceiver.kt
+++ app/src/main/java/com/icebem/akt/app/GestureActionReceiver.kt
@@ -1,29 +1,19 @@
-package com.icebem.akt.app;
+package com.icebem.akt.app
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-import com.icebem.akt.BuildConfig;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.icebem.akt.BuildConfig
 
 /**
- * 开始戳戳乐™本地广播接收器
+ * 戳戳乐™开始/结束行动本地广播接收器
  */
-public class GestureActionReceiver extends BroadcastReceiver {
-
-    /**
-     * Broadcast Action Name
-     */
-    public static final String ACTION = BuildConfig.APPLICATION_ID + ".START_ACTION";
-
-    private Runnable runnable;
-
-    public GestureActionReceiver(Runnable runnable) {
-        this.runnable = runnable;
+class GestureActionReceiver(private val runnable: Runnable) : BroadcastReceiver() {
+    companion object {
+        const val ACTION = "${BuildConfig.APPLICATION_ID}.START_ACTION"
     }
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        runnable.run();
+    override fun onReceive(context: Context, intent: Intent) {
+        runnable.run()
     }
 }
