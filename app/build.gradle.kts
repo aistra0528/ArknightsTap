7b8d5b47cc01391331ebc0c4f465f0adddeec5cc
IcebemAst <2369631276@qq.com>
2021-05-14 22:32:48 +0800

    build: 更新AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,4 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("com.google.android:flexbox:2.0.1")
     implementation("com.google.android.material:material:1.2.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
