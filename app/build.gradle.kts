3cb3d96aed6e87f7db21683044eb299c5593f05b
IcebemAst <2369631276@qq.com>
2022-10-31 13:27:12 +0800

    chore: 更新依赖库

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 }
 
 android {
-    compileSdk = 31
-    buildToolsVersion = "31.0.0"
     namespace = "com.icebem.akt"
+    compileSdk = 33
+    buildToolsVersion = "33.0.0"
 
     defaultConfig {
         applicationId = "com.icebem.akt"
@@ -16,31 +16,30 @@ android {
         versionName = "2.10.5"
         resourceConfigurations += arrayOf("zh-rCN", "en", "ja", "in")
     }
-    signingConfigs {
-        create("release") {
+    val signing = if (file("../signing.properties").exists()) {
+        signingConfigs.create("release") {
             val props = `java.util`.Properties().apply { load(file("../signing.properties").reader()) }
             storeFile = file(props.getProperty("storeFile"))
             storePassword = props.getProperty("storePassword")
             keyAlias = props.getProperty("keyAlias")
             keyPassword = props.getProperty("keyPassword")
         }
-    }
+    } else signingConfigs.getByName("debug")
     buildTypes {
         debug {
-            versionNameSuffix = "-Lune-${`java.text`.SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())}"
-            signingConfig = signingConfigs.getByName("release")
+            applicationIdSuffix = ".luna"
         }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signing
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     applicationVariants.all {
         outputs.all {
-            if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl)
-                outputFileName = "ArkTap-v$versionName.apk"
+            (this as? com.android.build.gradle.internal.api.ApkVariantOutputImpl)
+                    ?.outputFileName = "ArkTap-v$versionName.apk"
         }
     }
     kotlinOptions {
@@ -50,10 +49,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures {
-        viewBinding = false
-        dataBinding = false
-    }
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
@@ -61,15 +56,16 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation("androidx.core:core-ktx:1.7.0")
-    /** @Deprecated */
-    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
+    val kotlinVersion: String by rootProject
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.0")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.1")
+    /** @Deprecated */
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    val kotlinVersion by extra("1.7.20")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
